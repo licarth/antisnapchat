d6bb6837a23c60f3eb8858fc8bef41c379d1c803
licarth <thomascarli@gmail.com>
2013-12-03 13:14:37 +0100

    _

--- CropopAndroidApp/src/com/cropop/android/v1/service/CropopService.java
+++ CropopAndroidApp/src/com/cropop/android/v1/service/CropopService.java
@@ -1,9 +1,8 @@
 package com.cropop.android.v1.service;
 
-import java.util.Date;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import android.app.Service;
 import android.content.Intent;
@@ -20,47 +19,32 @@ public class CropopService extends Service {
 
 	public static int DETECTION_PERIOD = 2*1000;
 
+	final ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(1);
 
 	@Override
 	public void onCreate() {
 		// TODO Auto-generated method stub
 		super.onCreate();
 		android.os.Debug.waitForDebugger();
+		pool.scheduleAtFixedRate(new Runnable() {
+			@Override
+			public void run() {
+				Log.i("CropopService", "Service check Run !");
+//				checkForNewMessagesOnline();
+			}
+		}, 0, DETECTION_PERIOD, TimeUnit.SECONDS);
+		Log.i("CropopService", "Service started : active: "+pool.getActiveCount()+"scheduled"+pool.getQueue().size());
+//	pool.execute(command);
 	}
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		Log.i("CropopService", "Service started.");
-
-		final Timer timer = new Timer();
-
-//		timer.schedule(new TimerTask() {
-//
-//			synchronized public void run() {
-//				Log.i("CropopService", "Service check Run !");
-//				checkForNewMessagesOnline();
-//				timer.cancel();
-//				timer.schedule(this, new Date(System.currentTimeMillis() + 10000));
-//			}
-//
-//		}, new Date(System.currentTimeMillis() + 10000));
-		
-		timer.scheduleAtFixedRate(new TimerTask() {
-
-			synchronized public void run() {
-				Log.i("CropopService", "Service check Run !");
-				checkForNewMessagesOnline();
-			}
-
-		}, new Date(System.currentTimeMillis()), DETECTION_PERIOD);
-
 		return START_STICKY;
-
 	}
 
 	protected void checkForNewMessagesOnline() {
+		Log.i("CropopService", "Sending messages request...");
 		ParseUser currentUser = ParseUser.getCurrentUser();
-
 		//Parse Query : get all undelivered messages for me.
 		// Assume ParseObject myPost was previously created.
 		ParseQuery<ParseObject> query = ParseQuery.getQuery("Message");
@@ -80,34 +64,34 @@ public class CropopService extends Service {
 				message.setObjectId(parseObject.getObjectId());
 				db.addNewMessage(message);
 			}
-			
-			
+
+
 		} catch (ParseException e) {
-		
+
 		}
-		
-		
-//		query.findInBackground(new FindCallback<ParseObject>() {
-//			public void done(List<ParseObject> messageList, ParseException e) {
-//				Log.i("ParseQuery", String.format("Found %s messages", messageList.size()));
-//				for (ParseObject parseObject : messageList) {
-//					Message message = new Message();
-//					message.setContent(parseObject.getString("content"));
-//					message.setTarget(parseObject.getParseGeoPoint("target"));
-//					message.setDelivered(false);
-//					message.setDest_user(parseObject.getParseUser("dest_user"));
-//					message.setExp_user(parseObject.getParseUser("exp_user"));
-//					message.setObjectId(parseObject.getObjectId());
-//					DictionaryOpenHelper db = new DictionaryOpenHelper(CropopService.this);
-//					db.addNewMessage(message);
-//				}
-//			}
-//		});
+
+
+		//		query.findInBackground(new FindCallback<ParseObject>() {
+		//			public void done(List<ParseObject> messageList, ParseException e) {
+		//				Log.i("ParseQuery", String.format("Found %s messages", messageList.size()));
+		//				for (ParseObject parseObject : messageList) {
+		//					Message message = new Message();
+		//					message.setContent(parseObject.getString("content"));
+		//					message.setTarget(parseObject.getParseGeoPoint("target"));
+		//					message.setDelivered(false);
+		//					message.setDest_user(parseObject.getParseUser("dest_user"));
+		//					message.setExp_user(parseObject.getParseUser("exp_user"));
+		//					message.setObjectId(parseObject.getObjectId());
+		//					DictionaryOpenHelper db = new DictionaryOpenHelper(CropopService.this);
+		//					db.addNewMessage(message);
+		//				}
+		//			}
+		//		});
 
 
 	}
 
-	
+
 
 	@Override
 	public IBinder onBind(Intent intent) {
